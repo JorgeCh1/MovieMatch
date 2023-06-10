858dc4546e00307ab93963b6084a0e0c3b8fa712
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-06-10 11:14:02 -0600

    First Commit [Sidebar]

---
+++ MovieMatch/FrmSidebar.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace MovieMatch
+{
+    partial class FrmSidebar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSidebar));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnMenu = new System.Windows.Forms.PictureBox();
+            this.sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnHome = new System.Windows.Forms.Panel();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.pnWishlist = new System.Windows.Forms.Panel();
+            this.btnWishlist = new System.Windows.Forms.Button();
+            this.pnProfile = new System.Windows.Forms.Panel();
+            this.btnProfile = new System.Windows.Forms.Button();
+            this.pnLogout = new System.Windows.Forms.Panel();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.sidebarTransition = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMenu)).BeginInit();
+            this.sidebar.SuspendLayout();
+            this.pnHome.SuspendLayout();
+            this.pnWishlist.SuspendLayout();
+            this.pnProfile.SuspendLayout();
+            this.pnLogout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.btnMenu);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1035, 35);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnMenu
+            // 
+            this.btnMenu.Image = ((System.Drawing.Image)(resources.GetObject("btnMenu.Image")));
+            this.btnMenu.Location = new System.Drawing.Point(8, 5);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(36, 27);
+            this.btnMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMenu.TabIndex = 0;
+            this.btnMenu.TabStop = false;
+            this.btnMenu.Click += new System.EventHandler(this.BtnMenu_Click);
+            // 
+            // sidebar
+            // 
+            this.sidebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.sidebar.Controls.Add(this.label1);
+            this.sidebar.Controls.Add(this.pnHome);
+            this.sidebar.Controls.Add(this.pnWishlist);
+            this.sidebar.Controls.Add(this.pnProfile);
+            this.sidebar.Controls.Add(this.pnLogout);
+            this.sidebar.Dock = System.Windows.Forms.DockStyle.Left;
+            this.sidebar.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.sidebar.Location = new System.Drawing.Point(0, 35);
+            this.sidebar.Name = "sidebar";
+            this.sidebar.Size = new System.Drawing.Size(216, 658);
+            this.sidebar.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Minecraft", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(3, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(3, 50, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 56);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MovieMatch";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pnHome
+            // 
+            this.pnHome.Controls.Add(this.btnHome);
+            this.pnHome.Location = new System.Drawing.Point(3, 206);
+            this.pnHome.Margin = new System.Windows.Forms.Padding(3, 100, 3, 3);
+            this.pnHome.Name = "pnHome";
+            this.pnHome.Size = new System.Drawing.Size(217, 47);
+            this.pnHome.TabIndex = 2;
+            // 
+            // btnHome
+            // 
+            this.btnHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.btnHome.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.Location = new System.Drawing.Point(-8, -6);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.btnHome.Size = new System.Drawing.Size(247, 60);
+            this.btnHome.TabIndex = 3;
+            this.btnHome.Text = "       Home";
+            this.btnHome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHome.UseVisualStyleBackColor = false;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // pnWishlist
+            // 
+            this.pnWishlist.Controls.Add(this.btnWishlist);
+            this.pnWishlist.Location = new System.Drawing.Point(3, 286);
+            this.pnWishlist.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.pnWishlist.Name = "pnWishlist";
+            this.pnWishlist.Size = new System.Drawing.Size(217, 47);
+            this.pnWishlist.TabIndex = 4;
+            // 
+            // btnWishlist
+            // 
+            this.btnWishlist.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.btnWishlist.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWishlist.ForeColor = System.Drawing.Color.White;
+            this.btnWishlist.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnWishlist.Location = new System.Drawing.Point(-8, -6);
+            this.btnWishlist.Name = "btnWishlist";
+            this.btnWishlist.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.btnWishlist.Size = new System.Drawing.Size(247, 60);
+            this.btnWishlist.TabIndex = 3;
+            this.btnWishlist.Text = "       Wishlist";
+            this.btnWishlist.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnWishlist.UseVisualStyleBackColor = false;
+            this.btnWishlist.Click += new System.EventHandler(this.btnWishlist_Click);
+            // 
+            // pnProfile
+            // 
+            this.pnProfile.Controls.Add(this.btnProfile);
+            this.pnProfile.Location = new System.Drawing.Point(3, 366);
+            this.pnProfile.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.pnProfile.Name = "pnProfile";
+            this.pnProfile.Size = new System.Drawing.Size(217, 47);
+            this.pnProfile.TabIndex = 3;
+            // 
+            // btnProfile
+            // 
+            this.btnProfile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.btnProfile.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProfile.ForeColor = System.Drawing.Color.White;
+            this.btnProfile.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProfile.Location = new System.Drawing.Point(-8, -6);
+            this.btnProfile.Name = "btnProfile";
+            this.btnProfile.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.btnProfile.Size = new System.Drawing.Size(247, 60);
+            this.btnProfile.TabIndex = 3;
+            this.btnProfile.Text = "       Profile";
+            this.btnProfile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProfile.UseVisualStyleBackColor = false;
+            this.btnProfile.Click += new System.EventHandler(this.btnProfile_Click);
+            // 
+            // pnLogout
+            // 
+            this.pnLogout.Controls.Add(this.btnLogout);
+            this.pnLogout.Location = new System.Drawing.Point(3, 606);
+            this.pnLogout.Margin = new System.Windows.Forms.Padding(3, 190, 3, 3);
+            this.pnLogout.Name = "pnLogout";
+            this.pnLogout.Size = new System.Drawing.Size(217, 47);
+            this.pnLogout.TabIndex = 4;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(21)))), ((int)(((byte)(21)))), ((int)(((byte)(21)))));
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.Location = new System.Drawing.Point(-8, -6);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Padding = new System.Windows.Forms.Padding(25, 0, 0, 0);
+            this.btnLogout.Size = new System.Drawing.Size(247, 60);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "       Log Out";
+            this.btnLogout.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // sidebarTransition
+            // 
+            this.sidebarTransition.Interval = 10;
+            this.sidebarTransition.Tick += new System.EventHandler(this.sidebarTransition_Tick);
+            // 
+            // FrmSidebar
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(1035, 693);
+            this.Controls.Add(this.sidebar);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.IsMdiContainer = true;
+            this.Name = "FrmSidebar";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmSidebar_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnMenu)).EndInit();
+            this.sidebar.ResumeLayout(false);
+            this.pnHome.ResumeLayout(false);
+            this.pnWishlist.ResumeLayout(false);
+            this.pnProfile.ResumeLayout(false);
+            this.pnLogout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox btnMenu;
+        private System.Windows.Forms.FlowLayoutPanel sidebar;
+        private System.Windows.Forms.Panel pnHome;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnWishlist;
+        private System.Windows.Forms.Button btnWishlist;
+        private System.Windows.Forms.Panel pnProfile;
+        private System.Windows.Forms.Button btnProfile;
+        private System.Windows.Forms.Panel pnLogout;
+        private System.Windows.Forms.Button btnLogout;
+        private System.Windows.Forms.Timer sidebarTransition;
+    }
+}
+
---
+++ MovieMatch/FrmSidebar.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieMatch
+{
+    public partial class FrmSidebar : Form
+    {
+        bool sidebarExpand = true;
+        FrmHome home;
+        FrmWishlist wishlist;
+        FrmProfile profile;
+
+        public FrmSidebar()
+        {
+            InitializeComponent();
+            mdiProperties();
+        }
+
+        private void mdiProperties()
+        {
+            this.SetBevel(false);
+            Controls.OfType<MdiClient>().FirstOrDefault().BackColor = Color.FromArgb(232, 234, 237);
+        }
+        private void sidebarTransition_Tick(object sender, EventArgs e)
+        {
+            if (sidebarExpand)
+            {
+                sidebar.Width -= 5;
+                if (sidebar.Width <= 48)
+                {
+                    sidebarExpand = false;
+                    sidebarTransition.Stop();
+
+                    pnHome.Width = sidebar.Width;
+                    pnWishlist.Width = sidebar.Width;
+                    pnProfile.Width = sidebar.Width;
+                    pnLogout.Width = sidebar.Width;
+                }
+            }
+            else
+            {
+                sidebar.Width += 5;
+                if (sidebar.Width >= 216)
+                {
+                    sidebarExpand = true;
+                    sidebarTransition.Stop();
+
+                    pnHome.Width = sidebar.Width;
+                    pnWishlist.Width = sidebar.Width;
+                    pnProfile.Width = sidebar.Width;
+                    pnLogout.Width = sidebar.Width;
+                }
+            }
+
+        }
+
+        private void BtnMenu_Click(object sender, EventArgs e)
+        {
+            sidebarTransition.Start();
+        }
+
+        private void FrmSidebar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            if (home == null)
+            {
+                home = new FrmHome();
+                home.FormClosed += Home_FormClosed;
+                home.MdiParent = this;
+                home.Dock = DockStyle.Fill;
+                home.Show();
+            }
+            else
+            {
+                home.Activate();
+            }
+        }
+
+        private void Home_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            home = null;
+        }
+
+        private void btnWishlist_Click(object sender, EventArgs e)
+        {
+
+            if (wishlist == null)
+            {
+                wishlist = new FrmWishlist();
+                wishlist.FormClosed += Wishlist_FormClosed;
+                wishlist.MdiParent = this;
+                wishlist.Dock = DockStyle.Fill;
+                wishlist.Show();
+            }
+            else
+            {
+                wishlist.Activate();
+            }
+        }
+
+        private void Wishlist_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            wishlist = null;
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+
+            if (profile == null)
+            {
+                profile = new FrmProfile();
+                profile.FormClosed += Profile_FormClosed;
+                profile.MdiParent = this;
+                profile.Dock = DockStyle.Fill;
+                profile.Show();
+            }
+            else
+            {
+                profile.Activate();
+            }
+        }
+
+        private void Profile_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            profile = null;
+        }
+    }
+}
