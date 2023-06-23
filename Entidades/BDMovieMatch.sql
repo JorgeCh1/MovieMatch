fb88862e64c5f5277e4e481540266d385b7efc5a
JorgeCh1 <jorge.chavez13731@est.uca.edu.ni>
2023-06-23 14:43:23 -0600

    Base de datos añadida en Entidades

---
+++ Entidades/BDMovieMatch.sql
@@ -0,0 +1,91 @@
+ USE master
+go
+CREATE DATABASE BdPelis
+go
+USE BdPelis
+go
+/* 
+ * TABLE: Peliculas 
+ */
+
+CREATE TABLE Peliculas(
+    IdPelicula          int               IDENTITY(1,1),
+    Titulo              nvarchar(100)     NOT NULL,
+    Sinopsis            nvarchar(1000)    NOT NULL,
+    Rating              float             NULL,
+    FechaLanzamiento    date              NULL,
+    Generos             nvarchar(100)     NULL,
+    Poster              nvarchar(max)     NULL,
+    CONSTRAINT PK2 PRIMARY KEY NONCLUSTERED (IdPelicula)
+)
+go
+
+
+
+IF OBJECT_ID('Peliculas') IS NOT NULL
+    PRINT '<<< CREATED TABLE Peliculas >>>'
+ELSE
+    PRINT '<<< FAILED CREATING TABLE Peliculas >>>'
+go
+
+/* 
+ * TABLE: PeliculasUsuarios 
+ */
+
+CREATE TABLE PeliculasUsuarios(
+    IdUsuario     int    NOT NULL,
+    IdPelicula    int    NOT NULL,
+    CONSTRAINT PK10 PRIMARY KEY NONCLUSTERED (IdUsuario, IdPelicula)
+)
+go
+
+
+
+IF OBJECT_ID('PeliculasUsuarios') IS NOT NULL
+    PRINT '<<< CREATED TABLE PeliculasUsuarios >>>'
+ELSE
+    PRINT '<<< FAILED CREATING TABLE PeliculasUsuarios >>>'
+go
+
+/* 
+ * TABLE: Usuarios 
+ */
+
+CREATE TABLE Usuarios(
+    IdUsuario              int               IDENTITY(1,1),
+    PrimerNombre           nvarchar(50)      NOT NULL,
+    SegundoNombre          nvarchar(50)      NULL,
+    PrimerApellido         nvarchar(50)      NOT NULL,
+    SegundoApellido        nvarchar(50)      NULL,
+    Usuario                nvarchar(50)      NOT NULL,
+    Clave                  nvarchar(64)      NOT NULL,
+    CorreoElectronico      nvarchar(100)     NOT NULL,
+    FechaCreacionCuenta    date              NOT NULL,
+    Imagen                 varbinary(max)    NULL,
+    CONSTRAINT PK1 PRIMARY KEY NONCLUSTERED (IdUsuario)
+)
+go
+
+
+
+IF OBJECT_ID('Usuarios') IS NOT NULL
+    PRINT '<<< CREATED TABLE Usuarios >>>'
+ELSE
+    PRINT '<<< FAILED CREATING TABLE Usuarios >>>'
+go
+
+/* 
+ * TABLE: PeliculasUsuarios 
+ */
+
+ALTER TABLE PeliculasUsuarios ADD CONSTRAINT RefUsuarios8 
+    FOREIGN KEY (IdUsuario)
+    REFERENCES Usuarios(IdUsuario)
+go
+
+ALTER TABLE PeliculasUsuarios ADD CONSTRAINT RefPeliculas9 
+    FOREIGN KEY (IdPelicula)
+    REFERENCES Peliculas(IdPelicula)
+go
+
+
